622a33aedf6fe5583ce55ba72633c2fa4d9448b8
Dmitry Mikhaylov <dmitry.mikhaylow@gmail.com>
2013-06-18 17:09:09 +0400

    Uploading for ftp works now

--- letitbit.py
+++ letitbit.py
@@ -4,102 +4,146 @@ import httplib, urllib
 from ftplib import FTP
 import os
 
+
+class Error(Exception):
+    def __init__(self, value, msg=None):
+        Exception.__init__(self)
+        self.value = value
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class UnknownProtocolException(Error):
+    def __init__(self, value, msg=None):
+        Error.__init__(self, value, msg)
+        self.msg = msg if msg else "Unknown Protocol: \"{}\"".format(self.value)
+
+
+class NotSuccessfulResponseException(Error):
+    def __init__(self, value, msg=None):
+        Error.__init__(self, value, msg)
+        self.msg = msg if msg else "Not Successful Response came from server: \"{}\"".format(self.value)
+
+
 def empty(inList):
     if isinstance(inList, list):    # Is a list
-        return all( map(empty, inList) )
-    return False # Not a list
+        return all(map(empty, inList))
+    return False  # Not a list
+
 
 class Letitbit(object):
-    "Class for access to Letitbit.net file sharing services"
-    def __init__(self, private_key):
-        self.private_key = private_key
+    """Class to access to Letitbit.net file sharing services"""
+
+    protocols = ('ftp', 'http',)
+
+    def __init__(self, key, protocol='ftp'):
+        self.key = key
+        self.protocol = protocol
         
         self.headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
         self.conn = httplib.HTTPConnection("api.letitbit.net")
-        
-        self.data = [self.private_key]
+        self.data = [self.key]
+
+        self.servers = dict()
+        for p in Letitbit.protocols:
+            self.servers[p] = list()
+
+        self._get_auth_data(self.protocol)
+        self.get_servers_list(self.protocol)
 
     def add_method(self, controller, method, parameters=None):
-        meth = []
+        meth = list()
         meth.append("{}/{}".format(controller, method))
         if parameters:
             meth.append(parameters)
         self.data.append(meth)
 
     def run(self):
+        params = urllib.urlencode({'r': json.dumps(self.data)})
         response = None
         try:
-            print(json.dumps(self.data))
-            params = urllib.urlencode({'r': json.dumps(self.data)})
             self.conn.request("POST", "", params, self.headers)
             response = self.conn.getresponse()
             response = response.read()
             response = json.loads(response)
-        except:
-            print('Exception occured while tried to send request')
         finally:
-            # clean up response
-            self.data = [self.private_key]
+            self.data = [self.key]
         return response
 
     def check_key_info(self):
         self.add_method('key', 'info')
         result = self.run()
         if result['status'] != 'OK':
-                raise Exception("Error occured while trying to access API.\nConnection status: %s" % result['status'])
+            raise NotSuccessfulResponseException(result['status'])
         key_data = result['data'][0]
         self.max_requests = key_data['max']
         self.current_requests = key_data['cur']
+        self.total_requests = key_data['total_requests']
+        self.total_points = key_data['total_points']
 
-class Ftp(Letitbit):
-        "Class enables Ftp uploading functionalities"
-        def __init__(self, private_key):
-            Letitbit.__init__(self, private_key)
-            self._get_auth_data()
-            self._get_servers_list()
+    def get_servers_list(self, protocol):
+        if protocol not in Letitbit.protocols:
+            raise UnknownProtocolException(protocol)
+        self.add_method(protocol, 'listing')
+        result = self.run()
+        if result['status'] !='OK':
+            raise UnknownProtocolException(result['status'])
+        servers = result['data'][0]
+        servers = sorted(servers, key=lambda server: server[1])
+        s = []
+        for server in servers:
+            s.append(server[0])
+        self.servers[protocol] = servers
 
-        def _get_auth_data(self):
-            self.add_method('ftp', 'auth_data')
-            result = self.run()
-            if result['status'] != 'OK':
-                raise Exception("Error occured while trying to access API.\nConnection status: %s" % result['status'])
-            auth_data = result['data'][0]
+    def ftp_upload_file(self, file_full_path, server):
+        filename = os.path.basename(file_full_path)
+        file = open(file_full_path, "rb")
+        ftp = FTP(server)
+        ftp.login(self.login, self.password)
+        ftp.storbinary('STOR ' + filename, file)
+        ftp.quit()
+        file.close()
 
-            self.login = auth_data['login']
-            self.password = auth_data['pass']
+    def process(self, protocol, server, filename):
+        args = {
+            'server': server,
+            'tmpname': filename,
+            'realname': filename
+        }
+        self.add_method(protocol, 'process', args)
+        response = self.run()
+        print response
+        return response
 
-        def _get_servers_list(self):
-            self.add_method('ftp', 'listing')
-            result = self.run()
-            if result['status'] !='OK':
-                raise Exception("Error occured while trying to access API:\n%s" % result['status'])
-            ftp_servers = result['data'][0]
-            ftp_servers = sorted(ftp_servers, key=lambda server: server[1])
-            servers = []
-            for server in ftp_servers:
-                servers.append(server[0])
-            self.servers = servers
+    def upload_file(self, file_full_path, protocol):
+        """ Method allows uploading file to one of available servers depending on protocol you chose """
+        if protocol not in Letitbit.protocols:
+                raise UnknownProtocolException(protocol)
+        # get first server from list sorted by workload
+        server = self.servers[protocol][0][0]
 
-        def upload_file(self, file_full_path):
-            # get random server from servers list
-            import random
-            ftp_server = random.choice(self.servers)
-            filename = os.path.basename(file_full_path)
-            file = open(file_full_path, "rb")
-            ftp = FTP(ftp_server)
-            ftp.login(self.login, self.password)
-            ftp.storbinary('STOR ' + filename, file)
-            ftp.quit()
-            file.close()
-            args = {
-                'server': ftp_server,
-                'tmpname': filename,
-                'realname': filename
-            }
-            self.add_method('ftp', 'process', args)
-            response = self.run()
-            print response
-            if response['status'] != 'OK' or empty(response['data']):
-                raise Exception('File is not processed, maybe the server-side problem, try again')
-            return (response['data'][0][0]['link'], response['data'][0][0]['uid'])
+        if protocol == 'ftp':
+            self.ftp_upload_file(file_full_path, server)
+        else:
+            pass
+
+        filename = os.path.basename(file_full_path)
+        response = self.process(protocol, server, filename)
+        if response['status'] != 'OK' or empty(response['data']):
+            raise NotSuccessfulResponseException(response['status'])
+        return response['data'][0][0]['link'], response['data'][0][0]['uid']
+
+    def _get_auth_data(self, protocol):
+        if protocol not in Letitbit.protocols:
+            raise UnknownProtocolException(protocol)
+        self.add_method(protocol, 'auth_data')
+        result = self.run()
+        if result['status'] != 'OK':
+            raise NotSuccessfulResponseException(result['status'])
+        auth_data = result['data'][0]
+
+        self.login = auth_data['login']
+        self.password = auth_data['pass']
 
